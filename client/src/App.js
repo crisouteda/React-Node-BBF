fa9dd80868766ba7dd225c5eeea3d8e76ab9b856
crisouteda <cristina.outeda.rua@gmail.com>
2021-03-25 12:34:52 +0100

    from LocalStage to React.Context

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
@@ -7,26 +7,29 @@ import Footer from "./components/Footer";
 import Sign from "./components/Sign";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import { StateContext, UserContext } from "./components/State-context";
 // import Protected from "./components/Protected";
 function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (localStorage.getItem("loggedIn") !== null) {
-      setLoggedIn(localStorage.getItem("loggedIn"));
-    }
-  }, []);
+  const [logged, setLogged] = useState(false);
+  const [user, setUser] = useState(null);
 
   return (
     <Router>
       <div className="App">
-        <Navbar />
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/sign" exact component={Sign} />
-          <Route path="/main" exact component={Main} />
-          <Route path="/profile" render={(props) => <Profile {...props} />} />
-        </Switch>
+        <StateContext.Provider value={{ logged, setLogged }}>
+          <UserContext.Provider value={{ user, setUser }}>
+            <Navbar />
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/sign" exact component={Sign} />
+              <Route path="/main" exact component={Main} />
+              <Route
+                path="/profile"
+                render={(props) => <Profile {...props} />}
+              />
+            </Switch>
+          </UserContext.Provider>
+        </StateContext.Provider>
         <Footer />
       </div>
       {/* <Protected path="/profile" component={Profile} isAuth={loggedIn} /> */}
