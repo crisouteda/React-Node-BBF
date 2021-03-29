e0fe4cb3b80a60c1247a4b6f831c877710c9eab7
crisouteda <cristina.outeda.rua@gmail.com>
2021-03-29 16:52:17 +0200

    style added

--- client/src/components/LinkButton.js
+++ client/src/components/LinkButton.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withRouter } from "react-router";
+import { StyledButton } from "./Style";
 
 const LinkButton = (props) => {
   const {
@@ -14,7 +15,7 @@ const LinkButton = (props) => {
     ...rest
   } = props;
   return (
-    <button
+    <StyledButton
       {...rest} // `children` is just another prop!
       onClick={(event) => {
         onClick && onClick(event);
