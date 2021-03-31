e6ce6309250f2695b649a5e7522e5f1dd21998fc
crisouteda <cristina.outeda.rua@gmail.com>
2021-03-31 19:48:19 +0200

    sign splited

--- client/src/components/Sign/Sign.js
+++ client/src/components/Sign/Sign.js
@@ -1,104 +1,26 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from "react";
 import axios from "axios";
 import LinkButton from "../LinkButton";
-import { StyledButton, StyledCards, StyledCard } from "../Style";
+import { StyledCards, StyledCard } from "../Style";
 import { UserContext } from "../UserContext";
-
+import SignForm from "./Form";
 export default function Sign() {
-  const { user, dispatch } = useContext(UserContext);
-  const [emailUp, setEmailUp] = useState("");
-  const [passwordUp, setPasswordUp] = useState("");
-
-  const [emailIn, setEmailIn] = useState("");
-  const [passwordIn, setPasswordIn] = useState("");
+  const { user } = useContext(UserContext);
 
   axios.defaults.withCredentials = true;
-
-  const submitSignUp = () => {
-    axios
-      .post("http://localhost:4000/auth/signup", {
-        email: emailUp,
-        password: passwordUp,
-      })
-      .then((response) => {
-        console.log(response);
-      });
-  };
 
   const userAuthenticated = () => {
     axios.get("http://localhost:4000/auth/isUserAuth", {}).then((response) => {
       console.log(response);
     });
   };
-  const submitSignIn = () => {
-    axios
-      .post("http://localhost:4000/auth/signin", {
-        email: emailIn,
-        password: passwordIn,
-      })
-      .then((response) => {
-        if (!response.data.auth) {
-        } else {
-          const token = response.data.token;
-          const id = response.data.id;
-          dispatch({
-            type: "LOG",
-            user: {
-              token,
-              id,
-            },
-          });
-        }
-      });
-  };
-
   return (
     <StyledCards>
       <StyledCard>
-        <h1>Sign up</h1>
-        <div className="form">
-          <label>email</label>
-          <input
-            type="email"
-            name="email"
-            id="signInput"
-            onChange={(e) => {
-              setEmailUp(e.target.value);
-            }}
-          />
-          <label>password</label>
-          <input
-            type="password"
-            name="password"
-            id="signInput"
-            onChange={(e) => {
-              setPasswordUp(e.target.value);
-            }}
-          />
-          <StyledButton onClick={submitSignUp}>Submit</StyledButton>
-        </div>
+        <SignForm id="signUpInput" />
       </StyledCard>
       <StyledCard>
-        <h1>Sign in</h1>
-        <div className="form">
-          <label>email</label>
-          <input
-            type="email"
-            name="email"
-            onChange={(e) => {
-              setEmailIn(e.target.value);
-            }}
-          />
-          <label>password</label>
-          <input
-            type="password"
-            name="password"
-            onChange={(e) => {
-              setPasswordIn(e.target.value);
-            }}
-          />
-          <StyledButton onClick={submitSignIn}>Submit</StyledButton>
-        </div>
+        <SignForm id="signInInput" />
         {user[0].id !== null && (
           <LinkButton to="/profile" onClick={userAuthenticated}>
             Check authenticated
