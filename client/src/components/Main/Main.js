d897aebf6f5366869414e49f04efc51a07da3ede
crisouteda <cristina.outeda.rua@gmail.com>
2021-03-31 19:46:38 +0200

    main split

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,17 +1,16 @@
 import React, { useState, useEffect, useContext } from "react";
 import Axios from "axios";
 import { UserContext } from "../UserContext";
-import { StyledButton, StyledCards, StyledCard } from "../Style";
+import { StyledCards } from "../Style";
+import MovieForm from "./MovieForm";
+import UpdateForm from "./UpdateForm";
+import { CountContext } from "./CountContext";
 
 export default function Main() {
   const { user } = useContext(UserContext);
-  const [title, setTitle] = useState("");
-  const [author, setAuthor] = useState("");
+  const { count } = useContext(CountContext);
+
   const [authorList, setauthorList] = useState([]);
-  const [newAuthor, setNewAuthor] = useState("");
-  const [show, setShow] = useState(false);
-  const [input, setInput] = useState(false);
-  const [count, setCount] = useState(0);
 
   useEffect(() => {
     if (user[0].id) {
@@ -23,132 +22,11 @@ export default function Main() {
     }
   }, [count, user[0].id]);
 
-  const submitAuthor = () => {
-    Axios.post("http://localhost:4000/api/insert", {
-      title: title,
-      author: author,
-      user_id: user[0].id,
-    }).then(() => {
-      setCount(count + 1);
-    });
-  };
-
-  const deleteAuthor = (item) => {
-    Axios.delete(`http://localhost:4000/api/delete/${item}`).then(() => {
-      setCount(count + 1);
-    });
-  };
-
-  const updateAuthor = (props) => {
-    Axios.put("http://localhost:4000/api/update", {
-      id: props,
-      author: newAuthor,
-    }).then(() => {
-      setCount(count + 1);
-    });
-  };
   return (
     <StyledCards>
-      {show === false && (
-        <StyledCard>
-          <StyledButton onClick={() => setShow(!show)}>
-            {show === false && "Add review"}
-          </StyledButton>
-        </StyledCard>
-      )}
-      {show === true && (
-        <StyledCard>
-          <h1>Insert a movie</h1>
-          <div className="form">
-            <label>Movie name</label>
-            <input
-              type="text"
-              name="title"
-              onChange={(e) => {
-                setTitle(e.target.value);
-              }}
-            />
-            <label>Author</label>
-            <input
-              type="text"
-              name="author"
-              onChange={(e) => {
-                setAuthor(e.target.value);
-              }}
-            />
-            <div
-              style={{
-                display: "flex",
-                width: "100%",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <StyledButton
-                onClick={() => {
-                  submitAuthor();
-                }}
-              >
-                Submit
-              </StyledButton>
-              <StyledButton onClick={() => setShow(!show)}>
-                {show === true && "Cancel"}
-              </StyledButton>
-            </div>
-          </div>
-        </StyledCard>
-      )}
+      <MovieForm />
       {authorList.map((val) => {
-        return (
-          <StyledCard key={val.id}>
-            <h1>{val?.title}</h1>
-            <h2>{val?.author}</h2>
-            {input && (
-              <div>
-                <input
-                  type="text"
-                  id="updateInput"
-                  name="newAuthor"
-                  onChange={(e) => {
-                    setNewAuthor(e.target.value);
-                  }}
-                />
-                <StyledButton
-                  onClick={() => {
-                    updateAuthor(val.id);
-                  }}
-                >
-                  Update
-                </StyledButton>
-              </div>
-            )}
-            <div
-              style={{
-                display: "flex",
-                width: "100%",
-                flexDirection: "row",
-                justifyContent: "space-evenly",
-              }}
-            >
-              <StyledButton
-                onClick={() => {
-                  setInput(!input);
-                }}
-              >
-                {input === true ? "Cancel" : "Update"}
-              </StyledButton>
-
-              <StyledButton
-                // bottom="0px"
-                onClick={() => {
-                  deleteAuthor(val.id);
-                }}
-              >
-                Delete
-              </StyledButton>
-            </div>
-          </StyledCard>
-        );
+        return <UpdateForm key={val.id} val={val} />;
       })}
     </StyledCards>
   );
---
+++ client/src/components/Main/MovieForm.js
@@ -0,0 +1,74 @@
+import React, { useState, useContext } from "react";
+import Axios from "axios";
+import { StyledButton, StyledCard } from "../Style";
+import { UserContext } from "../UserContext";
+import { CountContext } from "./CountContext";
+
+function MovieForm({ id, ...rest }) {
+  const { user } = useContext(UserContext);
+  const { dispatch } = useContext(CountContext);
+  const [show, setShow] = useState(false);
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const submitAuthor = () => {
+    Axios.post("http://localhost:4000/api/insert", {
+      title: title,
+      author: author,
+      user_id: user[0].id,
+    }).then(() => {
+      dispatch({ type: "increment" });
+    });
+  };
+  return (
+    <StyledCard>
+      {show === false && (
+        <StyledButton onClick={() => setShow(!show)}>
+          {show === false && "Add review"}
+        </StyledButton>
+      )}
+      {show === true && (
+        <div className="form">
+          <h1>Insert a movie</h1>
+          <label>Movie name</label>
+          <input
+            type="text"
+            name="title"
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+          />
+          <label>Author</label>
+          <input
+            type="text"
+            name="author"
+            onChange={(e) => {
+              setAuthor(e.target.value);
+            }}
+          />
+          <div
+            style={{
+              display: "flex",
+              width: "100%",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <StyledButton
+              onClick={() => {
+                submitAuthor();
+              }}
+            >
+              Submit
+            </StyledButton>
+            <StyledButton onClick={() => setShow(!show)}>
+              {show === true && "Cancel"}
+            </StyledButton>
+          </div>
+        </div>
+      )}
+    </StyledCard>
+  );
+}
+
+export default MovieForm;
