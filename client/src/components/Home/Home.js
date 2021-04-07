ed8c0e52f0aba208408ba5f4e004e7cfc1f50d59
crisouteda <cristina.outeda.rua@gmail.com>
2021-04-07 11:06:49 +0200

    delete user from home

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,12 +1,10 @@
 import React, { useContext } from "react";
 import { StyledCards } from "../Style";
-import { UserContext } from "../UserContext";
 
 const Home = () => {
-  const { user } = useContext(UserContext);
   return (
     <StyledCards>
-      <div>Wellcome to the review organizer!!</div>
+      <div>Wellcome to the review organizer!</div>
     </StyledCards>
   );
 };
