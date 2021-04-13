e6bbb0c5605f95475a54265e55f88a4679dbfbf0
cristina <cristina.outeda.rua@gmail.com>
2021-04-13 09:18:18 +0200

    footer deleted

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,13 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
-import Footer from "./components/Footer/Footer";
 import Sign from "./components/Sign/Sign";
 import Home from "./components/Home/Home";
 import Profile from "./components/Profile/Profile";
 import { StyledApp } from "./components/Style";
 import UserProvider from "./components/UserContext";
 import ProtectedRoute from "./components/Protected";
-import CountProvider from "./components/Main/CountContext";
+import CountProvider from "./components/CountContext";
 
 function App() {
   return (
@@ -19,14 +18,13 @@ function App() {
         <StyledApp>
           <Navbar />
           <Switch>
-            <Route path="/" exact component={Home} />
             <Route path="/sign" exact component={Sign} />
             <CountProvider>
+              <Route path="/" exact component={Home} />
               <ProtectedRoute path="/main" Component={Main} />
+              <ProtectedRoute path="/profile" Component={Profile} />
             </CountProvider>
-            <ProtectedRoute path="/profile" Component={Profile} />
           </Switch>
-          {/* <Footer /> */}
         </StyledApp>
       </UserProvider>
     </Router>
