cb5caa5cec1b845c90f9d852ebd7fd217b12a449
cristina <cristina.outeda.rua@gmail.com>
2021-04-13 09:19:40 +0200

    coments deleted

--- client/src/components/UserContext.js
+++ client/src/components/UserContext.js
@@ -1,23 +1,3 @@
-// import React, { createContext, useEffect, useReducer } from "react";
-// import { userReducer } from "./reducers/userReducer";
-
-// export const UserContext = createContext();
-
-// const UserProvider = (props) => {
-//   const initialState =
-//     localStorage.getItem("user") || JSON.stringify([new Boolean(false)]);
-//   const [user, dispatch] = useReducer(userReducer, initialState);
-//   useEffect(() => {
-//     localStorage.setItem("user", JSON.stringify(user));
-//   }, [user]);
-//   return (
-//     <UserContext.Provider value={{ user, dispatch }}>
-//       {props.children}
-//     </UserContext.Provider>
-//   );
-// };
-// export default UserProvider;
-
 import React, { createContext, useEffect, useReducer } from "react";
 import { userReducer } from "./reducers/userReducer";
 
